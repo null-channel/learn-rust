0a2a39c530205ab01d9265f40f9606e779864ccd
Puneet <puneet2715@gmail.com>
2023-01-08 13:34:56 +0530

    copied bonus code to epsiode specific branch

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let number = 5;
-    let number2 = add(number,number);
+    let number2 = add(number, number);
     println!("The number is {number2}");
 }
 
-// 
+//
 pub fn add(int1: i32, int2: i32) -> i32 {
     return int1 + int2;
 }
@@ -14,3 +14,15 @@ fn add2(int1: i32, int2: i32) -> i32 {
     int1 + int2
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let number = 5;
+        let number2 = add(number, number);
+        assert_eq!(number2, number + number);
+        assert_ne!(number2, number - number);
+    }
+}

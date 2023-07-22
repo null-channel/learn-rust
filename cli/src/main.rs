cbcf9bd29abdc37277064df020519c9868ed0ace
Marek Counts <countsmarek@gmail.com>
2023-07-22 15:02:20 -0400

    updates

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,71 @@
 use std::{error::Error, fmt};
 
+trait Area {
+    fn area(&self) -> i32;
+}
+
+impl Area for String {
+    fn area(&self) -> i32 {
+        42
+    }
+}
+
+trait Height {
+    fn get_height(&self) -> i32;
+}
+
+trait Width {
+    fn get_width(&self) -> i32;
+}
+
+struct Square {
+    width: i32,
+}
+
+struct Rectangle {
+    width: i32,
+    height: i32,
+}
+
+enum Polygons {
+    Square(i32),
+    Rectangle(i32,i32),
+    EqTriangle{h: i32,w:i32}
+}
+
+impl Area for Polygons {
+    fn area(&self) -> i32 {
+        match self {
+            Polygons::Square(side_length) => {
+                *side_length * *side_length
+            },
+            Polygons::Rectangle(length,width) => {
+                *length * *width
+            },
+            Polygons::EqTriangle{h,w} => {
+                (*h * *w) / 2
+            }
+        }
+    }
+}
+
+impl Area for Rectangle {
+    fn area(&self) -> i32 {
+        self.height * self.width
+    }
+}
+
+impl Area for Square {
+    fn area(&self) -> i32 {
+        self.width * self.width
+    }
+}
+
+fn get_area<T>(x: T) -> i32 
+where T: Area {
+    x.area()
+}
+
 #[derive(Debug,PartialEq)]
 struct OldPoint {
     x: i32,
@@ -52,14 +118,15 @@ impl<T> Point<T> {
 }
 
 fn main() -> Result<(),Box<dyn Error>> {
-    let x = 5;
-    let old_point = OldPoint {x: 5, y: 10};
-    let new_point: Point<String> = Point {x: "5".to_string(), y: "10".to_string()};
-    println!("new point: {}", new_point);
-    let old_point_2 = OldPoint {x: 2, y: 2};
-    let new_point_2 = Point {x: old_point, y: old_point_2};
-    println!("point of PointOlds: {}", new_point_2);
-    print_point(new_point_2);
+
+    let poly = Polygons::EqTriangle { h: 5, w: 5 };
+
+    let rec = Rectangle{width: 5, height: 5};
+    
+    let areaRec = get_area(rec);
+
+    let areaPoly = get_area(poly);
+
     Ok(())
 }
 
